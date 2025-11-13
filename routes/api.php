4b7435af5605c15127a639af2ad58553ec07e65e
andikurniawan04 <andikurniawan.9a@gmail.com>
2025-11-13 08:48:36 +0700

    feat: add api order

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\TransactionController;
+use App\Http\Controllers\API\OrderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,4 +23,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::middleware('api-key')->group(function () {
     Route::post('/transaction/cancel-dso-order/{dsoORder}', [TransactionController::class, 'cancelDsoOrder']);
+    Route::resource('order', OrderController::class);
+    Route::get('update-order', [OrderController::class, 'updateOrder']);
+    Route::get('detail-order', [OrderController::class, 'detailOrder']);
 });
